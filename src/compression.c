8cc9ac26abd5657393a964bcb671e5af61a836e6
daimbk <daimbkhalid@gmail.com>
2023-12-24 16:23:23 +0500

    removed extra fork

--- src/compression.c
+++ src/compression.c
@@ -8,38 +8,19 @@
 #include "globals.h"
 #include "compression.h"
 
-int executeCommand(const char *command)
-{
-    pid_t child_pid = fork();
-
-    if (child_pid == -1)
-    {
-        perror("Error: Fork Failed");
-        return -1;
-    }
-
-    if (child_pid == 0)
-    {
-        // Child process
-        execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
-        perror("Error: exec");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        // Parent process
-        int status;
-        waitpid(child_pid, &status, 0);
-        return status;
-    }
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 int compressFile(const char *filePath)
 {
     char command[PATH_MAX + 20]; // Maximum command length: "gzip -f filename"
     snprintf(command, sizeof(command), "gzip -f %s", filePath);
 
-    return executeCommand(command);
+    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
+    perror("Error: exec");
+    exit(EXIT_FAILURE);
 }
 
 int decompressFile(const char *filePath)
@@ -47,7 +28,9 @@ int decompressFile(const char *filePath)
     char command[PATH_MAX + 24]; // Maximum command length: "gunzip -f filename.gz"
     snprintf(command, sizeof(command), "gunzip -f %s", filePath);
 
-    return executeCommand(command);
+    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
+    perror("Error: exec");
+    exit(EXIT_FAILURE);
 }
 
 int compressFolder(const char *folderPath, const char *zipFilePath)
@@ -55,7 +38,9 @@ int compressFolder(const char *folderPath, const char *zipFilePath)
     char command[PATH_MAX + PATH_MAX + 50]; // Maximum command length: "zip -r zipFileName folderPath"
     snprintf(command, sizeof(command), "zip -r %s %s", zipFilePath, folderPath);
 
-    return executeCommand(command);
+    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
+    perror("Error: exec");
+    exit(EXIT_FAILURE);
 }
 
 int decompressZip(const char *zipFilePath, const char *extractPath)
@@ -63,5 +48,7 @@ int decompressZip(const char *zipFilePath, const char *extractPath)
     char command[PATH_MAX + PATH_MAX + 50]; // Maximum command length: "unzip -d extractPath zipFileName"
     snprintf(command, sizeof(command), "unzip -d %s %s", extractPath, zipFilePath);
 
-    return executeCommand(command);
+    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
+    perror("Error: exec");
+    exit(EXIT_FAILURE);
 }
