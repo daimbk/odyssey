36e6b771307351677c39381db594bf24df29c3c4
emeika <shahbazhafsa2014@gmail.com>
2023-12-25 23:03:41 +0500

    fix: auto exit on zip

--- src/compression.c
+++ src/compression.c
@@ -1,54 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <limits.h>
 
 #include "globals.h"
 #include "compression.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-
 int compressFile(const char *filePath)
 {
-    char command[PATH_MAX + 20]; // Maximum command length: "gzip -f filename"
+    char command[PATH_MAX + 20];
     snprintf(command, sizeof(command), "gzip -f %s", filePath);
 
-    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
-    perror("Error: exec");
-    exit(EXIT_FAILURE);
+    if (system(command) == -1)
+    {
+        perror("Error: system");
+        return -1;
+    }
+
+    return 0;
 }
 
 int decompressFile(const char *filePath)
 {
-    char command[PATH_MAX + 24]; // Maximum command length: "gunzip -f filename.gz"
+    char command[PATH_MAX + 24];
     snprintf(command, sizeof(command), "gunzip -f %s", filePath);
 
-    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
-    perror("Error: exec");
-    exit(EXIT_FAILURE);
+    if (system(command) == -1)
+    {
+        perror("Error: system");
+        return -1;
+    }
+
+    return 0;
 }
 
 int compressFolder(const char *folderPath, const char *zipFilePath)
 {
-    char command[PATH_MAX + PATH_MAX + 50]; // Maximum command length: "zip -r zipFileName folderPath"
+    char command[PATH_MAX + PATH_MAX + 50];
     snprintf(command, sizeof(command), "zip -r %s %s", zipFilePath, folderPath);
 
-    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
-    perror("Error: exec");
-    exit(EXIT_FAILURE);
+    if (system(command) == -1)
+    {
+        perror("Error: system");
+        return -1;
+    }
+
+    return 0;
 }
 
 int decompressZip(const char *zipFilePath, const char *extractPath)
 {
-    char command[PATH_MAX + PATH_MAX + 50]; // Maximum command length: "unzip -d extractPath zipFileName"
+    char command[PATH_MAX + PATH_MAX + 50];
     snprintf(command, sizeof(command), "unzip -d %s %s", extractPath, zipFilePath);
 
-    execl("/bin/sh", "/bin/sh", "-c", command, (char *)NULL);
-    perror("Error: exec");
-    exit(EXIT_FAILURE);
+    if (system(command) == -1)
+    {
+        perror("Error: system");
+        return -1;
+    }
+
+    return 0;
 }
