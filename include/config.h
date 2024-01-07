975d0ac82970aa7f021c8a8c59ddd48947511994
Daim Bin Khalid <51926730+daimbk@users.noreply.github.com>
2024-01-07 17:54:01 +0500

    prompt color changing (#26)
    * feat: prompt color customization
    
    * refactor: prompt color functions

--- include/config.h
+++ include/config.h
@@ -1,16 +1,25 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <stdbool.h>
+
+#include "globals.h"
+
 #define CONFIG_FOLDER_NAME ".odyssey"
 #define CONFIG_FILE_NAME "shellconfig.txt"
 
-#include <stdbool.h>
-
 // -------CONFIG VARIABLES-------
 extern bool show_ascii_art;
+extern char *usernameColor;
+extern char *hostnameColor;
+extern char *currentDirColor;
 
 void createConfigFolder();
 void loadConfig();
 void toggle_ascii_art(char *cmd);
+
+void setUsernameColor(char *color);
+void setHostnameColor(char *color);
+void setCurrentDirColor(char *color);
 
 #endif
